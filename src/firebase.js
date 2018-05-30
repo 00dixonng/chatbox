03d704f845a758829f0712133881709e31e8de96
Dixon Ng <00.dixon.ng@gmail.com>
2018-05-30 14:43:32 +1000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 // src/firebase.js
 import firebase from 'firebase'
 const config = {
-    apiKey: "AIzaSyBIGZBZuCUsmSr3J7rGs7uxOlS9ecXmpTg",
-    authDomain: "fun-food-friends-3c456.firebaseapp.com",
-    databaseURL: "https://fun-food-friends-3c456.firebaseio.com",
-    projectId: "fun-food-friends-3c456",
-    storageBucket: "fun-food-friends-3c456.appspot.com",
-    messagingSenderId: "347566686985"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 };
 firebase.initializeApp(config);
 export default firebase;
